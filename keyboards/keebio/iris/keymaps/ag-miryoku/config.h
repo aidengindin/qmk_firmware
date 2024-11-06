89da423cc395e66395eaf2799b26c5ff27def25e
Aiden Gindin <aiden@thegindins.com>
2024-11-06 16:41:32 -0500

    Update miryoku layout for iris keyboard

--- keyboards/keebio/iris/keymaps/ag-miryoku/config.h
+++ keyboards/keebio/iris/keymaps/ag-miryoku/config.h
@@ -1,20 +1,28 @@
 #pragma once
 
-#include "config_common.h"
-
-// this is important for home row mods, idk
-#define IGNORE_MOD_TAP_INTERRUPT
-
-#define MK_KINETIC_SPEED
+// Prevent normal rollover on alphas from accidentally triggering mods.
+// #define IGNORE_MOD_TAP_INTERRUPT
 
 #define TAPPING_TERM 200
 
-// trying to reduce firmware size
+// #define MK_KINETIC_SPEED
+
+// Mouse key speed and acceleration.
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY          0
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL       16
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY    0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED      6
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX    64
+
+// reduce firmware size
 #define NO_ACTION_ONESHOT
-// #undef RGBLIGHT_EFFECT_ALTERNATING
-// #undef RGBLIGHT_EFFECT_CHRISTMAS
-// #undef RGBLIGHT_EFFECT_RAINBOW_MOOD
-// #undef RGBLIGHT_EFFECT_RAINBOW_SWIRL
-// #undef RGBLIGHT_EFFECT_RGB_TEST
-// #undef RGBLIGHT_EFFECT_STATIC_GRADIENT
-// #undef RGBLIGHT_EFFECT_SNAKE
+
+#define SPLIT_USB_DETECT
+#define SPLIT_USB_TIMEOUT 2000
+#define SPLIT_USB_TIMEOUT_POLL 10
+#define SPLIT_WATCHDOG_ENABLE
--- keyboards/keebio/iris/keymaps/ag-miryoku/keymap.c
+++ keyboards/keebio/iris/keymaps/ag-miryoku/keymap.c
@@ -1,65 +1,76 @@
 #include QMK_KEYBOARD_H
 
-
-/* #define _QWERTY 0
-#define _SYMBOL 1
-#define _ADJUST 2 */
-
-#define _BASE  0
-#define _NUM   1
-#define _SYMB  2
-#define _FUN   3
-#define _NAV   4
-#define _MEDIA 5
-#define _MOUSE 6
+#define _BASE 0
+#define _NAV 1
+#define _MOUSE 2
+#define _MEDIA 3
+#define _NUM 4
+#define _SYM 5
+#define _FUN 6
 
 enum custom_keycodes {
-  MS_JGL = SAFE_RANGE,
+    MS_JGL = SAFE_RANGE,
 };
 
 bool mouse_jiggle_mode = false;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  /* [_QWERTY] = LAYOUT(
-     KC_CAPS,  KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                               KC_6,    KC_7,           KC_8,           KC_9,           KC_0,           KC_MINS,
-     KC_GRV,   KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                               KC_Y,    KC_U,           KC_I,           KC_O,           KC_P,           KC_MINS,
-     KC_ESC,   GUI_T(KC_A),    ALT_T(KC_S),    CTL_T(KC_D),    SFT_T(KC_F),    KC_G,                               KC_H,    SFT_T(KC_J),    CTL_T(KC_K),    ALT_T(KC_L),    GUI_T(KC_SCLN), KC_QUOT,
-     KC_LPRN,  KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,    KC_DEL,          CW_TOGG,  KC_N,    KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,        KC_RPRN,
-                                                               MO(_SYMBOL),  KC_TAB,   KC_BSPC,           KC_SPC,  KC_ENT,  MO(_ADJUST)
-  ),
+    [_BASE] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
+        _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______,
+        _______, GUI_T(KC_A), ALT_T(KC_S), CTL_T(KC_D), SFT_T(KC_F), KC_G,  KC_H,    SFT_T(KC_J), CTL_T(KC_K), ALT_T(KC_L), GUI_T(KC_SCLN), _______,
+        _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
+        LT(_MEDIA,KC_ESC), LT(_NAV,KC_SPC), LT(_MOUSE,KC_TAB),                    LT(_SYM,KC_ENT), LT(_NUM,KC_BSPC), LT(_FUN,KC_DEL)
+    ),
 
-  [_SYMBOL] = LAYOUT(
-     KC_F1,   KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_F6,                              KC_F7,   KC_F8,          KC_F9,          KC_F10,         KC_F11,         KC_F12,
-     KC_PGUP, KC_AT,          KC_PERC,        KC_LPRN,        KC_RPRN,        KC_EQL,                             KC_PAST, KC_P7,          KC_P8,          KC_P9,          KC_AMPR,        KC_HOME,
-     KC_PGDN, GUI_T(KC_HASH), ALT_T(KC_DLR),  CTL_T(KC_LBRC), SFT_T(KC_RBRC), KC_PMNS,                            KC_PLUS, SFT_T(KC_P4),   CTL_T(KC_P5),   ALT_T(KC_P6),   GUI_T(_______), KC_END,
-     KC_PIPE, KC_LABK,        KC_RABK,        KC_LCBR,        KC_RCBR,        KC_PSLS, QK_BOOT,          KC_UP,   KC_P0,   KC_P1,          KC_P2,          KC_P3,          KC_PDOT,        KC_BSLS,
-                                                                     _______, KC_NUM,  _______,          KC_DOWN, KC_LEFT, KC_RGHT
-  ),
+    [_NAV] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                     KC_CAPS, _______, _______, _______, QK_BOOT,  _______,
+        _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,                    CW_TOGG, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______,  KC_INS,  KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______,
+        _______, _______, _______,                                               _______, _______, _______
+    ),
 
-  [_ADJUST] = LAYOUT(
-     RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI,                            RGB_TOG, RGB_M_P, RGB_M_B, RGB_MOD, RGB_RMOD, _______,
-     KC_BRIU, _______, KC_BTN2, KC_MS_U, KC_BTN1, KC_VOLU,                            KC_MNXT, _______, KC_WH_U, _______, DM_PLY2,  DM_REC2,
-     KC_BRID, _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_VOLD,                            KC_MPLY, KC_WH_L, KC_WH_D, KC_WH_R, DM_PLY1,  DM_REC1,
-     _______, MS_JGL,  KC_BTN3, KC_BTN3, _______, KC_MUTE, _______,          _______, KC_MPRV, _______, _______, _______, _______,  DM_RSTP,
-                                         KC_WBAK, KC_WFWD, _______,          _______, _______, _______
-  ) */
+    [_MOUSE] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                     _______, MS_JGL,  _______, _______, _______, _______,
+        _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,                     KC_BTN4, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______,   KC_BTN5, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______,
+        _______, _______, _______,                                               KC_BTN2, KC_BTN1, KC_BTN3
+    ),
 
-  [_BASE] = LAYOUT(
-    _______, _______,     _______,     _______,     _______,             _______,                                              _______,           _______,     _______,     _______,     _______,        _______,
-    _______, KC_Q,        KC_W,        KC_E,        KC_R,                KC_T,                                                 KC_Y,              KC_U,        KC_I,        KC_O,        KC_P,           _______,
-    _______, GUI_T(KC_A), ALT_T(KC_S), CTL_T(KC_D), SFT_T(KC_F),         KC_G,                                                 KC_H,              SFT_T(KC_J), CTL_T(KC_K), ALT_T(KC_L), GUI_T(KC_QUOT), _______,
-    _______, KC_Z,        KC_X,        KC_C,        KC_V,                KC_B,             _______,          _______,          KC_N,              KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,        _______,
-                                                    LT(_MOUSE, KC_TAB), LT(_NUM, KC_BSPC), LT(_NAV, KC_DEL), LT(_FUN, KC_ENT), LT(_SYMB, KC_SPC), LT(_MEDIA, KC_ESC)
-  ),
+    [_MEDIA] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                     RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, _______,
+        _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,                    RGB_RMOD, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______,   RGB_TOG, RGB_VAD, RGB_VAI, _______, _______, _______,
+        _______, _______, _______,                                               KC_MSTP, KC_MPLY, KC_MUTE
+    ),
 
-  [_NUM] = LAYOUT(
-    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-    _______, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_7,    KC_8,   KC_9,     KC_GRV,  _______,
-    _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,                     KC_EQL,  KC_4,    KC_5,   KC_6,     KC_SCLN, _______,
-    _______, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   _______, _______, KC_BSLS, KC_1,    KC_2,   KC_3,     KC_NO,   _______,
-                                        KC_NO,   KC_NO,   KC_NO,   KC_MINS, KC_0,    KC_NO
-  ),
+    [_NUM] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
+        _______, KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  _______,
+        _______, KC_QUOT, KC_4,    KC_5,    KC_6,    KC_EQL,                      XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, _______,
+        _______, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS, _______, _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        KC_DOT,  KC_0,    KC_MINS,                                               _______, _______, _______
+    ),
+
+    [_SYM] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
+        _______, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  _______,
+        _______, KC_DQUO, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,                     XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, _______,
+        _______, KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE, _______, _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        KC_LPRN, KC_RPRN, KC_UNDS,                                               _______, _______, _______
+    ),
+
+    [_FUN] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
+        _______, KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  _______,
+        _______, KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_SCRL,                     XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, _______,
+        _______, KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_PAUS, _______, _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        KC_APP,  KC_SPC,  KC_TAB,                                                _______, _______, _______
+    )
 };
 
 void matrix_scan_user(void) {
